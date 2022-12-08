8a9c144e9eaeb01fa7c2ffedd65ff33f1206e605
SanuthiVihansa <subasofts@gmail.com>
2022-12-08 06:26:25 +0530

    commit mongodb configurations

--- Backend/server.js
+++ Backend/server.js
@@ -11,3 +11,12 @@ const PORT = process.env.PORT || 8070;
 
 app.use(cors());
 app.use(bodyParse.json()); //json has key value pairs
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+    useCreateIndex: true, //creating an index 
+    useNewUrlParse: true,
+    useUnifiedTopologyL :true,
+    useFindAndModify: false
+})
