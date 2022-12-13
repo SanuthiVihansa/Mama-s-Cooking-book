17199e7206de55820897b6d90b9fde3a41e24ce9
SanuthiVihansa <subasofts@gmail.com>
2022-12-13 19:32:41 +0530

    commit server.js changes

--- Backend/server.js
+++ Backend/server.js
@@ -18,9 +18,9 @@ mongoose.connect(URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-
-const connection = mongoose.connection;
 //connecting to db once
+const connection = mongoose.connection;
+
 connection.once("open",() =>{
     console.log("Mongodb connection success!")
 });
